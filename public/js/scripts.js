5cde640a8876b647294d7a667d6892477060b933
rovinei <ro.vinei@yahoo.com>
2018-05-20 21:58:23 +0700

    commit scripts.js

---
+++ public/js/scripts.js
@@ -0,0 +1,18 @@
+var DP;
+if(!DP) DP = {};
+if(!DP.main) DP.main = {};
+
+(function($) {
+	var func = DP.main;
+
+	
+})(jQuery);
+var DP;
+if(!DP) DP = {};
+if(!DP.utils) DP.utils = {};
+
+(function($) {
+	var func = DP.utils;
+
+	
+})(jQuery);
