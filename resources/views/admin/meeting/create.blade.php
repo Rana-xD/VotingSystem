2815251a59382a9233fdabfd5f499b424f73fcfd
vanchhay <vanchhay>
2018-05-22 14:34:49 +0700

    fix add resolution form not toggling-modal

--- resources/views/admin/meeting/create.blade.php
+++ resources/views/admin/meeting/create.blade.php
@@ -179,6 +179,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="addUserForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+
 	<div class="modal-dialog modal-lg" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
@@ -190,10 +191,10 @@
 			<div class="modal-body">
 
 				<div class="container">	
-					<ul class="nav nav-pills">
-						<li class="nav-item">
+					<ul class="nav">
+						<li class="">
 							<a  href="#addNewUser" data-toggle="tab">Add New User</a>
-						</li class="nav-item">
+						</li class="">
 						<li><a href="#addExistingUser" data-toggle="tab">Add Existing User</a>
 						</li>
 					</ul>
@@ -283,47 +284,47 @@
 			</div>
 		</div>
 	</div>
+</div>
 
-	<div class="modal fade" id="addResolutionForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-lg" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body">
-					<div class="content">
-						<div class="container">
-							<div class="row">
-								<div class="col">
-									<div class="card">
-										<div class="card-header card-header-primary">
-											<h4 class="card-title">Add Resolution</h4>
-										</div>
-										<div class="card-body">
-											<form ">
-												<div class="row" id="resolutionQuestionEntry">
-													<div class="col-md-12 resolutionParent">
-														<div class="form-group">
-															<label >Resolution</label>
-															<input type="text" class="form-control resolutionQuestionInput">
-														</div>
+<div class="modal fade" id="addResolutionForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div class="content">
+					<div class="container">
+						<div class="row">
+							<div class="col">
+								<div class="card">
+									<div class="card-header card-header-primary">
+										<h4 class="card-title">Add Resolution</h4>
+									</div>
+									<div class="card-body">
+										<form ">
+											<div class="row" id="resolutionQuestionEntry">
+												<div class="col-md-12 resolutionParent">
+													<div class="form-group">
+														<label >Resolution</label>
+														<input type="text" class="form-control resolutionQuestionInput">
 													</div>
 												</div>
+											</div>
 
 
-												<br/><br/>
-												<div class="row">
-													<div class="col">
-														<button type="submit" class="btn btn-danger pull-right" data-dismiss="modal">Save</button>
-														<button id="btnAddResolution" type="button" class="btn btn-primary pull-right">Add more</button>
-													</div>
+											<br/><br/>
+											<div class="row">
+												<div class="col">
+													<button type="submit" class="btn btn-danger pull-right" data-dismiss="modal">Save</button>
+													<button id="btnAddResolution" type="button" class="btn btn-primary pull-right">Add more</button>
 												</div>
+											</div>
 
-												<div class="clearfix"></div>
-											</form>
-										</div>
+											<div class="clearfix"></div>
+										</form>
 									</div>
 								</div>
 							</div>
@@ -333,87 +334,88 @@
 			</div>
 		</div>
 	</div>
+</div>
 
-	
-	@includeIf('admin.partials.filemanager_dialog')	
-	@endsection
-	@section('execute_script')
-	<script type="text/javascript" src="{{ asset('/admins/plugins/tinymce/tinymce.min.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('/admins/plugins/tinymce/tinymce-config.js') }}"></script>
-	<script>
-		function responsive_filemanager_callback(field_id){
-			var imageUrl="",
-			imgArr = [],
-			domain = "{{ URL('/') }}";
-			switch(field_id){
-				case 'txtFeaturedImage':
-				imageUrl = $('#'+field_id).val();
-				imageUrl = imageUrl.replace(domain,'');
-				$('#imagePreviewDiv').empty().append(
-					'<img src="'+imageUrl+'" style="width:100%; margin-bottom:10px;">'
-					);
-				break;
-				case 'txtMultiImages':
-				imageUrl = $('#'+field_id).val();
-				imageUrl = imageUrl.replace(domain,'');
-				imgArr.push(imageUrl);
-				$('#slideImagesPreviewDiv img').each(function(i,k,v){
-					var imgSrc = $(this).attr('src');
-					imgArr.push(imgSrc);
-				});
-				$('#slideImgs').val(JSON.stringify(imgArr));
-				$('#slideImagesPreviewDiv').append(''+
-					'<div class="img_slide__outer">'+
-					'<img src="'+imageUrl+'" style="width:100%; margin-bottom:10px;">'+
-					'<span class="btnRmSlideImg">'+
-					'<i class="fa fa-remove"></i>'+
-					'</span>'+
-					'</div>'+
-					'');
-				break;
-				case 'txtMultiImages':
-				imageUrl = $('#'+field_id).val();
-				imageUrl = imageUrl.replace(domain,'');
-				imgArr.push(imageUrl);
-				$('#slideImagesPreviewDiv img').each(function(i,k,v){
-					var imgSrc = $(this).attr('src');
-					imgArr.push(imgSrc);
-				});
-				$('#slideImgs').val(JSON.stringify(imgArr));
-				$('#slideImagesPreviewDiv').append(''+
-					'<div class="img_slide__outer">'+
-					'<img src="'+imageUrl+'" style="width:100%; margin-bottom:10px;">'+
-					'<span class="btnRmSlideImg">'+
-					'<i class="fa fa-remove"></i>'+
-					'</span>'+
-					'</div>'+
-					'');
-				break;
-				case 'sound_url':
-				var playing = false,
-				audioEle = $('#audioEle').bind('play', function () {
-					playing = true;
-				}).bind('pause', function () {
-					playing = false;
-				}).bind('ended', function () {
-					audio.pause();
-				}).get(0);
-				var supportsAudio = !!document.createElement('audio').canPlayType;
-				if (supportsAudio){
-					$(audioEle).attr('src', $('#'+field_id).val());
-				}
-				break;
 
-				default:
-				return;
-
+@includeIf('admin.partials.filemanager_dialog')	
+@endsection
+@section('execute_script')
+<script type="text/javascript" src="{{ asset('/admins/plugins/tinymce/tinymce.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/admins/plugins/tinymce/tinymce-config.js') }}"></script>
+<script>
+	function responsive_filemanager_callback(field_id){
+		var imageUrl="",
+		imgArr = [],
+		domain = "{{ URL('/') }}";
+		switch(field_id){
+			case 'txtFeaturedImage':
+			imageUrl = $('#'+field_id).val();
+			imageUrl = imageUrl.replace(domain,'');
+			$('#imagePreviewDiv').empty().append(
+				'<img src="'+imageUrl+'" style="width:100%; margin-bottom:10px;">'
+				);
+			break;
+			case 'txtMultiImages':
+			imageUrl = $('#'+field_id).val();
+			imageUrl = imageUrl.replace(domain,'');
+			imgArr.push(imageUrl);
+			$('#slideImagesPreviewDiv img').each(function(i,k,v){
+				var imgSrc = $(this).attr('src');
+				imgArr.push(imgSrc);
+			});
+			$('#slideImgs').val(JSON.stringify(imgArr));
+			$('#slideImagesPreviewDiv').append(''+
+				'<div class="img_slide__outer">'+
+				'<img src="'+imageUrl+'" style="width:100%; margin-bottom:10px;">'+
+				'<span class="btnRmSlideImg">'+
+				'<i class="fa fa-remove"></i>'+
+				'</span>'+
+				'</div>'+
+				'');
+			break;
+			case 'txtMultiImages':
+			imageUrl = $('#'+field_id).val();
+			imageUrl = imageUrl.replace(domain,'');
+			imgArr.push(imageUrl);
+			$('#slideImagesPreviewDiv img').each(function(i,k,v){
+				var imgSrc = $(this).attr('src');
+				imgArr.push(imgSrc);
+			});
+			$('#slideImgs').val(JSON.stringify(imgArr));
+			$('#slideImagesPreviewDiv').append(''+
+				'<div class="img_slide__outer">'+
+				'<img src="'+imageUrl+'" style="width:100%; margin-bottom:10px;">'+
+				'<span class="btnRmSlideImg">'+
+				'<i class="fa fa-remove"></i>'+
+				'</span>'+
+				'</div>'+
+				'');
+			break;
+			case 'sound_url':
+			var playing = false,
+			audioEle = $('#audioEle').bind('play', function () {
+				playing = true;
+			}).bind('pause', function () {
+				playing = false;
+			}).bind('ended', function () {
+				audio.pause();
+			}).get(0);
+			var supportsAudio = !!document.createElement('audio').canPlayType;
+			if (supportsAudio){
+				$(audioEle).attr('src', $('#'+field_id).val());
 			}
+			break;
 
-			$("#fileManagerModal").modal('hide');
+			default:
+			return;
 
 		}
-		jQuery(document).ready(function($) {
-			$('#AddMeeting__form').on('submit', DP.main.addMeetingFormSubmitHandler);
+
+		$("#fileManagerModal").modal('hide');
+
+	}
+	jQuery(document).ready(function($) {
+		$('#AddMeeting__form').on('submit', DP.main.addMeetingFormSubmitHandler);
 			// Add Resolution handler
 			$('#btnAddResolution').on('click', DP.main.addResolutionQuestion);
 		});
